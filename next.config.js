ced9b9d555408307f23f9f6213641212883822e5
vishnuemail96 <vishnuemail9@gmail.com>
2025-04-01 14:04:28 +0530

    Next js changed

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["randomuser.me"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
   },
 };
 
